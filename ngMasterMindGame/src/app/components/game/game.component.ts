2e6581034c3d27359de01fb8a09c88009aa6c6bf
thaodev <71045132+thaodev@users.noreply.github.com>
2023-01-05 20:52:25 +0700

    create front end for attempt

--- ngMasterMindGame/src/app/components/game/game.component.ts
+++ ngMasterMindGame/src/app/components/game/game.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
+  isNewGame:boolean = false;
+  digit1: any;
+  digit2: any;
+  digit3: any;
+  digit4: any;
+  guess : number[] | null = null;
+  gameId: String | null = null;
+  constructor() {
 
-  constructor() { }
+   }
 
   ngOnInit(): void {
+    this.guess?.push(this.digit1);
+    this.guess?.push(this.digit2);
+    this.guess?.push(this.digit3);
+    this.guess?.push(this.digit4);
+  }
+
+  startGame(){
+    this.isNewGame = true;
+  }
+
+  checkAttempt() {
+
   }
 
 }
