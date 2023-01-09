8c80318e5411cce6fe821e980e4eadea8ec84185
thaodev <71045132+thaodev@users.noreply.github.com>
2023-01-09 07:48:22 +0700

    create Hint object and add it to Game properties

---
+++ src/main/java/com/mindGame/model/Hint.java
@@ -0,0 +1,30 @@
+package com.mindGame.model;
+
+public class Hint {
+	private int sumDigit;
+	private boolean isFirstEven;
+	private boolean isThirdDivideByThreeEqually;
+	
+	
+	public int getSumDigit() {
+		return sumDigit;
+	}
+	public void setSumDigit(int sumDigit) {
+		this.sumDigit = sumDigit;
+	}
+	public boolean isFirstEven() {
+		return isFirstEven;
+	}
+	public void setFirstEven(boolean isFirstEven) {
+		this.isFirstEven = isFirstEven;
+	}
+	public boolean isThirdDivideByThreeEqually() {
+		return isThirdDivideByThreeEqually;
+	}
+	public void setThirdDivideByThreeEqually(boolean isThird) {
+		this.isThirdDivideByThreeEqually = isThird;
+	}
+	
+	
+
+}
